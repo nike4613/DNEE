bd9850fb4e73f515baf18d42a44593db4d69c540
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-09-03 01:55:18 -0500

    Began to add internal types

---
+++ BSEventsSystem/Internal/DynamicInvokedEvent.cs
@@ -0,0 +1,30 @@
+﻿using BSEventsSystem.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSEventsSystem.Internal
+{
+    internal sealed class DynamicInvokedEvent : IEvent, IEventWithResult
+    {
+        public DynamicInvokedEvent(EventName name)
+        {
+            EventName = name;
+        }
+
+        public EventName EventName { get; }
+
+        private Maybe<dynamic?> result = Maybe.None;
+        public dynamic? Result { set => result = Maybe.Some(value); }
+        public bool DidCallNext { get; private set; } = false;
+
+        public EventResult Next(dynamic data)
+        {
+            DidCallNext = true;
+            throw new NotImplementedException();
+        }
+
+        public EventResult GetEventResult()
+            => result.HasValue ? new EventResult(result.Value) : default;
+    }
+}
---
+++ BSEventsSystem/Internal/IEventWithResult.cs
@@ -0,0 +1,19 @@
+﻿using BSEventsSystem.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSEventsSystem.Internal
+{
+    internal interface IEventWithResult
+    {
+        EventResult GetEventResult();
+
+        bool DidCallNext { get; }
+    }
+
+    internal interface IEventWithResult<T> : IEventWithResult
+    {
+        new EventResult<T> GetEventResult();
+    }
+}
